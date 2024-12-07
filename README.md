259ee587ee2d4d6f231b7b8c6234466eff0d0146
lidongxun967 <1985409711@qq.com>
2024-12-07 09:01:45 +0800

    feat(counter): 添加自定义增量功能并优化界面
    - 在数据文件中添加 plusn 字段，用于存储自定义增量值
    - 实现自定义增量功能，用户可以通过输入框设置增量值
    - 优化界面布局，调整按钮样式和位置
    - 更新 README 文件，简述新功能
    - 版本号升级到 0.3.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Flet Counter
 
-一个基于Flet的简单计数器应用，在原版的基础上添加了持久化存储及重置功能。
+一个基于Flet的简单计数器应用，在原版的基础上添加了一堆功能。
 
 [下载最新版](https://github.com/lidongxun967/flet-counter/releases/latest)
--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ def read_json_file(file_path):
     # Check if the file exists
     if not os.path.exists(file_path):
         # If file does not exist, create it and write {"counter": 0}
-        data = {"counter": 0}
+        data = {"counter": 0,"plusn":1}
         with open(file_path, "w") as file:
             json.dump(data, file)
     else:
@@ -18,7 +18,7 @@ def read_json_file(file_path):
         with open(file_path, "r") as file:
             data = json.load(file)
 
-    return data["counter"]
+    return data
 
 
 def update_json_file(file_path, update_value):
@@ -28,37 +28,51 @@ def update_json_file(file_path, update_value):
 
     # Write the updated data back to the file
     with open(file_path, "w") as file:
-        json.dump({"counter": update_value}, file)
+        json.dump(update_value, file)
 
 
 def main(page: ft.Page):
     page.title = "Counter"
     data_dir = os.getenv("FLET_APP_STORAGE_DATA") + "/data.json"
-
-    counter = ft.Text(read_json_file(data_dir), size=50)
+    counter = ft.Text(read_json_file(data_dir)["counter"], size=50)
 
     def plus(n):
         def increment_click(e):
-            v = read_json_file(data_dir) + n
+            v = read_json_file(data_dir)
+            v["counter"] += n
             update_json_file(data_dir, v)
-            counter.value = str(v)
+            counter.value = str(v["counter"])
+            counter.update()
+
+        return increment_click
+
+    def iptplus(iptn):
+        def increment_click(e):
+            v = read_json_file(data_dir)
+            v["counter"] += int(iptn.value)
+            v["plusn"] = int(iptn.value)
+            update_json_file(data_dir, v)
+            counter.value = str(v["counter"])
             counter.update()
 
         return increment_click
 
     def re(e):
-        update_json_file(data_dir, 0)
+        v = read_json_file(data_dir)
+        v["counter"] = 0
+        update_json_file(data_dir, v)
         counter.value = "0"
         counter.update()
 
-    page.add(ft.Text(f"\n{data_dir}"))
-    page.add(
-        ft.Button(
-            "RESET",
-            on_click=re,
-            style=ft.ButtonStyle(color=ft.colors.RED_50, bgcolor=ft.colors.RED),
-        )
+    ipt = ft.TextField(
+        label="输入数字",
+        value=read_json_file(data_dir)["plusn"],
+        width=100,
+        input_filter=ft.InputFilter(
+            allow=True, regex_string=r"^[0-9]*$", replacement_string=""
+        ),
     )
+    page.add(ft.Text(f"\n缓存文件路径：{data_dir}"))
     page.add(
         ft.SafeArea(
             ft.Column(
@@ -74,9 +88,32 @@ def main(page: ft.Page):
                     ),
                     ft.Row(
                         [
-                            ft.Button("+100", on_click=plus(100),style=ft.ButtonStyle(bgcolor=ft.colors.GREEN_900)),
-                            ft.Button("+10"  , on_click=plus(10)  ,style=ft.ButtonStyle(bgcolor=ft.colors.GREEN_800)),
-                            ft.Button("+1"    , on_click=plus(1)    ,style=ft.ButtonStyle(bgcolor=ft.colors.GREEN_700)),
+                            ft.Button(
+                                "+100",
+                                on_click=plus(100),
+                                style=ft.ButtonStyle(bgcolor=ft.Colors.GREEN_900),
+                            ),
+                            ft.Button(
+                                "+10",
+                                on_click=plus(10),
+                                style=ft.ButtonStyle(bgcolor=ft.Colors.GREEN_800),
+                            ),
+                            ft.Button(
+                                "+1",
+                                on_click=plus(1),
+                                style=ft.ButtonStyle(bgcolor=ft.Colors.GREEN_700),
+                            ),
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+                    ft.Row(
+                        [
+                            ft.FloatingActionButton(
+                                icon=ft.Icons.ADD,
+                                on_click=iptplus(ipt),
+                                bgcolor=ft.Colors.GREEN,
+                            ),
+                            ipt,
                         ],
                         alignment=ft.MainAxisAlignment.CENTER,
                     ),
@@ -84,7 +121,12 @@ def main(page: ft.Page):
                 alignment=ft.MainAxisAlignment.CENTER,
             ),
             expand=True,
-        )
+        ),
+        ft.Button(
+            "重置",
+            on_click=re,
+            style=ft.ButtonStyle(color=ft.Colors.RED_50, bgcolor=ft.Colors.RED),
+        ),
     )
 
 
