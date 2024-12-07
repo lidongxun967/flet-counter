99217cc7276f4f3cecd656e06ab6d6c4cdac6e7e
lidongxun967 <1985409711@qq.com>
2024-12-07 11:42:58 +0800

    feat(counter): 升级计数器功能并优化用户界面
    - 将计数器版本从 0.3.0 升级到 0.4.0
    - 重构了 JSON 文件读写逻辑，提高代码复用性
    - 新增调试对话框，方便用户查看和重置数据文件
    - 扩展了计数器功能，支持加、减、乘、除操作
    - 优化了用户界面布局，提高了可读性和易用性

---
+++ src/json_tool.py
@@ -0,0 +1,40 @@
+import os
+import json
+
+DATA = {"counter": 0,"n":1,"o":"+"}
+
+def read_json_file(file_path):
+    """
+    Reads data from a JSON file. If the file does not exist, it creates it with {"counter": 0}.
+    """
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        # If file does not exist, create it and write {"counter": 0}
+        with open(file_path, "w") as file:
+            json.dump(DATA, file)
+    else:
+        # If file exists, read from it
+        with open(file_path, "r") as file:
+            data = json.load(file)
+
+    return data
+
+
+def update_json_file(file_path, update_value):
+    """
+    Updates the "counter" value in the JSON file.
+    """
+
+    # Write the updated data back to the file
+    with open(file_path, "w") as file:
+        json.dump(update_value, file)
+
+# 初始化文件
+def reset_json_file(file_path):
+    """
+    Reads data from a JSON file. If the file does not exist, it creates it with {"counter": 0}.
+    """
+    # Check if the file exists=
+        # If file does not exist, create it and write {"counter": 0}
+    with open(file_path, "w") as file:
+        json.dump(DATA, file)
--- src/main.py
+++ src/main.py
@@ -1,79 +1,127 @@
 import flet as ft
 import os
-import json
-
-
-def read_json_file(file_path):
-    """
-    Reads data from a JSON file. If the file does not exist, it creates it with {"counter": 0}.
-    """
-    # Check if the file exists
-    if not os.path.exists(file_path):
-        # If file does not exist, create it and write {"counter": 0}
-        data = {"counter": 0,"plusn":1}
-        with open(file_path, "w") as file:
-            json.dump(data, file)
-    else:
-        # If file exists, read from it
-        with open(file_path, "r") as file:
-            data = json.load(file)
-
-    return data
-
-
-def update_json_file(file_path, update_value):
-    """
-    Updates the "counter" value in the JSON file.
-    """
-
-    # Write the updated data back to the file
-    with open(file_path, "w") as file:
-        json.dump(update_value, file)
+import json_tool
 
 
 def main(page: ft.Page):
     page.title = "Counter"
     data_dir = os.getenv("FLET_APP_STORAGE_DATA") + "/data.json"
-    counter = ft.Text(read_json_file(data_dir)["counter"], size=50)
+    v = json_tool.read_json_file(data_dir)
 
-    def plus(n):
+    def open_dlg(e):
+        def reset_click(e):
+            json_tool.reset_json_file(data_dir)
+        
+        dlg = ft.AlertDialog(
+            title=ft.Text("Debug Info"),
+            content=ft.Column(
+                [
+                    ft.Text("Data file url: "),
+                    ft.TextField(
+                        data_dir,
+                        multiline=True,
+                        read_only=True,
+                    ),
+                    ft.Text("Data file content: "),
+                    ft.TextField(
+                        json_tool.read_json_file(data_dir),
+                        multiline=True,
+                        read_only=True,
+                    ),ft.Text("完全重置"),
+                    ft.Button("确定", on_click=reset_click,style=ft.ButtonStyle(text_style=ft.TextStyle(size=30),shape=ft.RoundedRectangleBorder(radius=10),bgcolor=ft.Colors.RED, color=ft.Colors.WHITE)),
+                ],
+                height=300,
+            ),
+        )
+        page.open(dlg)
+
+    def calc(c, n):
+
+        # 静态
         def increment_click(e):
-            v = read_json_file(data_dir)
-            v["counter"] += n
-            update_json_file(data_dir, v)
+            v = json_tool.read_json_file(data_dir)
+            if c == "+":
+                v["counter"] += n
+            elif c == "*":
+                v["counter"] *= n
+            elif c == "/":
+                v["counter"] //= n
+            elif c == "-":
+                v["counter"] -= n
+            json_tool.update_json_file(data_dir, v)
+            counter.value = str(v["counter"])
+            counter.update()
+
+        return increment_click
+
+    def dcalc(c, n):
+        # 对于固定增加值的点击事件
+        def increment_click(e):
+            v = json_tool.read_json_file(data_dir)
+            if c.value == "+":
+                v["counter"] += int(n.value)
+            elif c.value == "*":
+                v["counter"] *= int(n.value)
+            elif c.value == "/":
+                v["counter"] //= int(n.value)
+            elif c.value == "-":
+                v["counter"] -= int(n.value)
+            v["n"] = int(n.value)
+            v["o"] = c.value
+            json_tool.update_json_file(data_dir, v)
             counter.value = str(v["counter"])
             counter.update()
 
         return increment_click
 
     def iptplus(iptn):
+        # 对于动态增加值的点击事件
         def increment_click(e):
-            v = read_json_file(data_dir)
+            v = json_tool.read_json_file(data_dir)
             v["counter"] += int(iptn.value)
             v["plusn"] = int(iptn.value)
-            update_json_file(data_dir, v)
+            json_tool.update_json_file(data_dir, v)
             counter.value = str(v["counter"])
             counter.update()
 
         return increment_click
 
     def re(e):
-        v = read_json_file(data_dir)
+        # 重置按钮的点击事件
+        v = json_tool.read_json_file(data_dir)
         v["counter"] = 0
-        update_json_file(data_dir, v)
+        json_tool.update_json_file(data_dir, v)
         counter.value = "0"
         counter.update()
 
-    ipt = ft.TextField(
-        label="输入数字",
-        value=read_json_file(data_dir)["plusn"],
+    # 预注册组件
+    itt = ft.Dropdown(
+        width=50,
+        value=v["o"],
+        options=[
+            ft.dropdown.Option("+"),
+            ft.dropdown.Option("-"),
+            ft.dropdown.Option("*"),
+            ft.dropdown.Option("/"),
+        ],
+    )
+    it = ft.TextField(
+        label="",
+        value=v["n"],
         width=100,
         input_filter=ft.InputFilter(
             allow=True, regex_string=r"^[0-9]*$", replacement_string=""
         ),
     )
-    page.add(ft.Text(f"\n缓存文件路径：{data_dir}"))
+    counter = ft.Text(json_tool.read_json_file(data_dir)["counter"], size=100)
+
+    # 页面构建
     page.add(
+        ft.ElevatedButton(
+            "调试",
+            on_click=open_dlg,
+            style=ft.ButtonStyle(bgcolor=ft.Colors.BLUE, color=ft.Colors.WHITE),
+        ),
         ft.SafeArea(
             ft.Column(
                 [
@@ -90,31 +138,24 @@ def main(page: ft.Page):
                         [
                             ft.Button(
                                 "+100",
-                                on_click=plus(100),
+                                on_click=calc("+", 100),
                                 style=ft.ButtonStyle(bgcolor=ft.Colors.GREEN_900),
                             ),
                             ft.Button(
                                 "+10",
-                                on_click=plus(10),
+                                on_click=calc("+", 10),
                                 style=ft.ButtonStyle(bgcolor=ft.Colors.GREEN_800),
                             ),
                             ft.Button(
                                 "+1",
-                                on_click=plus(1),
+                                on_click=calc("+", 1),
                                 style=ft.ButtonStyle(bgcolor=ft.Colors.GREEN_700),
                             ),
                         ],
                         alignment=ft.MainAxisAlignment.CENTER,
                     ),
                     ft.Row(
-                        [
-                            ft.FloatingActionButton(
-                                icon=ft.Icons.ADD,
-                                on_click=iptplus(ipt),
-                                bgcolor=ft.Colors.GREEN,
-                            ),
-                            ipt,
-                        ],
+                        [itt, it, ft.Button("=", on_click=dcalc(itt, it))],
                         alignment=ft.MainAxisAlignment.CENTER,
                     ),
                 ],
